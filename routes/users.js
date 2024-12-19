ca5998e967d90fd6fc2c0b8edad69971bdee4969
keennathan <keennathan@icloud.com>
2024-12-19 18:35:41 +1000

    feat: Parameters fetch from the URL

--- routes/users.js
+++ routes/users.js
@@ -15,18 +15,61 @@ router.get("/new", (req, res) => {
     });
 });
 
-router.get("/:id", (req, res) => {
+// Chain all the requests with the same URL pattern
+router.route("/:id")
+.get((req, res)=> {
+    // Function logic executed
+    // To fetch the query parameter: req.params.id
+    res.json({
+        message: `Getting a specific user detail with id: ${req.params.id}`
+    });
+})
+.patch((req, res)=> {
     // Function logic executed
     res.json({
-        message: "Getting a specific user detail"
+        message: `Editing a specific user detail with id: ${req.params.id}`
+    });
+})
+.put((req, res)=> {
+    // Function logic executed
+    res.json({
+        message: `Changing a specific user detail with id: ${req.params.id}`
+    });
+})
+.delete((req, res)=> {
+    // Function logic executed
+    res.json({
+        message: `Deleting a specific user detail with id: ${req.params.id}`
     });
 });
 
-router.patch("/:id", (req, res) => {
-    // Function logic executed
-    res.json({
-        message: "Editing something specific from the specific user."
-    });
-});
+// router.get("/:id", (req, res) => {
+//     // Function logic executed
+//     // To fetch the query parameter: req.params.id
+//     res.json({
+//         message: `Getting a specific user detail with id ${req.params.id}`
+//     });
+// });
+
+// router.patch("/:id", (req, res)=> {
+//     // Function logic executed
+//     res.json({
+//         message: `Editing a specific user detail with id: ${req.params.id}`
+//     });
+// });
+
+// router.put("/:id", (req, res)=> {
+//     // Function logic executed
+//     res.json({
+//         message: `Changing a specific user detail with id: ${req.params.id}`
+//     });
+// });
+
+// router.delete("/:id", (req, res)=> {
+//     // Function logic executed
+//     res.json({
+//         message: `Deleting a specific user detail with id: ${req.params.id}`
+//     });
+// });
 
 module.exports = router;
